00cd373491ff905a5216ddbe37b1381680cf9a50
juliana <julianasantostav@gmail.com>
2024-07-05 16:39:19 -0300

    feat: setup project

---
+++ class-practices/methods.cs
@@ -0,0 +1,17 @@
+using System; 
+
+namespace MeuApp
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é legal");
+        }
+    }
+}
